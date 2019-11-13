d649312f075741cd477a44f914837d364cbbf681
Jonathan Opio <jonathanopio@gmail.com>
2019-11-13 17:50:59 -0500

    service page

--- AttendenceScanner/src/app/home/home.page.ts
+++ AttendenceScanner/src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { BarcodeScanResult, BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { Firebase } from '@ionic-native/firebase/ngx';
+import { AttendanceLog, AttendanceLogService } from '../services/attendancelog.service';
 
 
 
