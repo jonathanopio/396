5b01fce9a49f4b3596f47105d0694196faf934e9
TwistedDealer <charlienewton@live.com>
2019-11-19 16:10:05 -0500

    broke but close

--- DatabaseTest1/src/app/login/login.page.ts
+++ DatabaseTest1/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string = ""
+  password: string = ""
+
+  constructor(public afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  async login() {
+    const {username, password } = this
+    try{
+      const res = await this.afAuth.auth.signInWithEmailAndPassword(username +'@students.calvin.edu', password)
+
+    }catch(err){
+      console.dir(err)
+      if(err.code === "auth/user-not-found"){
+        console.log("User not found")
+      }
+
+    }
   }
 
 }
