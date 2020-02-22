4645b1554e7ca62c86dbb72eca21c3b31fdb650c
TwistedDealer <charlienewton@live.com>
2020-02-22 18:04:18 -0500

    Working prototype

---
+++ qr_base/android/app/src/main/kotlin/com/a384/qr_base/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.a384.qr_base
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
